830efcffae33fcb53347d1d4de171c28827314bd
Mirko <giancarlo1234522@gmail.com>
2024-04-07 19:12:18 -0500

    update routes provides

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, createContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SignUp from './assets/components/SignUp';
 import Landinpage from './pages/landingPage/LandingPage';
@@ -6,20 +7,42 @@ import Account from './assets/components/Account';
 import Home from './pages/Home';
 import { MyProfile } from './pages/MyProfile';
 
+const AppContext = createContext();
+const { Provider: AppProvider, Consumer } = AppContext;
+
+import { getAuth } from 'firebase/auth';
+
 function App() {
+  const auth = getAuth();
+  const [currentUser, setCurrentUser] = useState(auth.currentUser || null);
+  const [dataOfUser, setDataOfUser] = useState(null);
+
+  useEffect(() => {
+    console.log('hola mundo');
+    return () => {};
+  }, [auth, dataOfUser, currentUser]);
+
+  const appValue = {
+    setCurrentUser,
+    currentUser,
+    setDataOfUser,
+    dataOfUser,
+  };
   return (
     <>
-      <AuthContextProvider>
-        <Routes>
-          <Route path="/" element={<Landinpage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/myProfile" element={<MyProfile />} />
-        </Routes>
-      </AuthContextProvider>
+      <AppProvider value={appValue}>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={<Landinpage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/myProfile" element={<MyProfile />} />
+          </Routes>
+        </AuthContextProvider>
+      </AppProvider>
     </>
   );
 }
 
-export default App;
+export { App, Consumer, AppContext };
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { App } from './App.jsx';
 import { BrowserRouter as BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from './pages/theme/Theme.js';
